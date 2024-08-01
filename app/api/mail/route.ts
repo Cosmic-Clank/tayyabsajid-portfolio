51932eb60c28c608a458f7518e1f64acad4ee3d9
Tayyab Sajid <tayyabsajidq41321@gmail.com>
2024-08-01 19:16:06 +0400

    testing

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -74,6 +74,6 @@ export async function GET(request: NextRequest) {
 		return Response.redirect(new URL("/mail/success", request.nextUrl));
 	} catch (error: any) {
 		// return Response.redirect(new URL("/mail/fail", request.nextUrl));
-		return { message: error.message };
+		return Response.json({ message: error.message });
 	}
 }
